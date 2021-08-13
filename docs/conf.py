e114c10172a87e004dc29008f13c9ab54244e2ed
jshwi <stephen@jshwisolutions.com>
2021-08-13 13:34:37 +1000

    Bump version: 3.2.1 → 3.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "3.2.1"
+release = "3.2.2"
 
 
 # -- General configuration ---------------------------------------------------
--- pyaud/__init__.py
+++ pyaud/__init__.py
@@ -3,7 +3,7 @@ from . import config, exceptions, plugins
 from ._environ import load_namespace
 from ._utils import branch, files, get_packages, git, package
 
-__version__ = "3.2.1"
+__version__ = "3.2.2"
 
 __all__ = [
     "branch",
