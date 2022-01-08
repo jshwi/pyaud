2c003258fce2a9d99690c6d5c5f3441157166eb5
jshwi <stephen@jshwisolutions.com>
2022-01-09 04:37:13 +1100

    Bump version: 3.7.0 → 3.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "3.7.0"
+release = "3.8.0"
 
 
 # -- General configuration ---------------------------------------------------
