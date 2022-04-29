86ebb1fd2da36d5cf6ce7276e519f2017599c153
jshwi <stephen@jshwisolutions.com>
2022-04-29 21:57:17 +1000

    Bump version: 3.13.2 → 3.13.3

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "3.13.2"
+release = "3.13.3"
 
 
 # -- General configuration ---------------------------------------------------
