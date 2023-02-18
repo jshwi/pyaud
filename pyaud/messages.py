5e3bbf91e95b03cba4d6af047e6f2cd1a0687543
jshwi <stephen@jshwisolutions.com>
2023-02-18 11:48:08 +1100

    change: Update audit messages for consistency
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- pyaud/messages.py
+++ pyaud/messages.py
@@ -2,8 +2,8 @@
 pyaud.messages
 ==============
 """
-AUDIT_PASSED = "Success: All checks have passed"
-AUDIT_FAILED = "Failed: One or more checks have failed"
+AUDIT_PASSED = "Success: all checks have passed"
+AUDIT_FAILED = "Failed: one or more checks have failed"
 AUDIT_RUNNING = "running the following plugins"
 TYPE_ERROR = "can only register one of the following: {valid}; not {invalid}"
 FAILED = "Failed: returned non-zero exit status {returncode}"
