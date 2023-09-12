91c6990d9b6b4364afc8f04cec77008886fbfbad
Stephen Whitlock <stephen@jshwisolutions.com>
2023-09-12 11:32:50 +1000

    bump: version 7.0.0 → 7.1.0
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "7.0.0"
+release = "7.1.0"
 
 
 # -- General configuration ---------------------------------------------------
