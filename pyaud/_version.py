501316bc8d1259fb2de5187d50bea6cfe518141e
Stephen Whitlock <stephen@jshwisolutions.com>
2023-11-30 17:35:00 +1100

    bump: version 7.2.2 → 8.0.0
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- pyaud/_version.py
+++ pyaud/_version.py
@@ -7,4 +7,4 @@ Contains the version of this package.
 Allows for access to the version internally without cyclic imports
 caused by accessing it through __init__.
 """
-__version__ = "7.2.2"
+__version__ = "8.0.0"
